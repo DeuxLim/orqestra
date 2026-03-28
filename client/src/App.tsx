94d56c2f50e455fc0b94fc18c6e334d5afe46559
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-28 19:02:48 +0800

    router setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import { RouterProvider } from "react-router";
+import { routes } from "@/routes";
+
 function App() {
-	return <div>Test</div>;
+	return (
+		<div>
+			<RouterProvider router={routes} />
+		</div>
+	);
 }
 
 export default App;
