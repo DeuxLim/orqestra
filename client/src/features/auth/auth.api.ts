5e7fc6278d0f28f2ea67a21ded676a440b4cd768
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-30 17:31:52 +0800

    register api endpoint

--- client/src/features/auth/auth.api.ts
+++ client/src/features/auth/auth.api.ts
@@ -1,9 +1,18 @@
 import api from "@/lib/api";
-import type { LoginInputType } from "@/features/auth/auth.types";
+import type {
+    LoginInputType,
+    RegisterInputType,
+} from "@/features/auth/auth.types";
 import { getCsrfToken } from "@/lib/csrf";
 
 export const login = async (data: LoginInputType) => {
     await getCsrfToken();
     const response = await api.post("/login", data);
+    return response;
+};
+
+export const register = async (data: RegisterInputType) => {
+    await getCsrfToken();
+    const response = await api.post("/register", data);
     return response;
 };
