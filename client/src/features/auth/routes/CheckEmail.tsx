77749bd67390ba34032b425ffe5a0ec826f1b4e6
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-30 19:46:16 +0800

    check email layout

---
+++ client/src/features/auth/routes/CheckEmail.tsx
@@ -0,0 +1,41 @@
+import AuthLayout from "@/components/layouts/AuthLayout";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useMutation } from "@tanstack/react-query";
+
+export default function CheckEmail() {
+    const { mutate, isPending, isSuccess } = useMutation({
+        mutationKey: ["resend-email"],
+    });
+    const handleResendEmail = () => {
+        mutate();
+    };
+    return (
+        <AuthLayout>
+            <Card className="w-110 text-center">
+                <CardHeader>
+                    <CardTitle>Please check your email</CardTitle>
+                </CardHeader>
+
+                <CardContent className="space-y-8">
+                    <p className="text-sm text-muted-foreground">
+                        We sent you a verification link. Please check your
+                        inbox.
+                    </p>
+
+                    <Button
+                        onClick={handleResendEmail}
+                        className={`w-full ${isPending ? "bg-yellow-300" : isSuccess ? "bg-green-300" : ""}`}
+                        variant="outline"
+                    >
+                        {isPending
+                            ? "Sending email verification link..."
+                            : isSuccess
+                              ? "Email verification link sent!"
+                              : "Resend Email"}
+                    </Button>
+                </CardContent>
+            </Card>
+        </AuthLayout>
+    );
+}
