8a063083c1183ca1b7b52049e60fe06cd96f4f35
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-30 17:32:09 +0800

    auth type

--- client/src/features/auth/auth.types.ts
+++ client/src/features/auth/auth.types.ts
@@ -1,1 +1,5 @@
-export type LoginInputType = { email: string; password: string };
+import type z from "zod";
+import type { LoginInputSchema, RegisterInputSchema } from "./auth.schema";
+
+export type LoginInputType = z.input<typeof LoginInputSchema>;
+export type RegisterInputType = z.input<typeof RegisterInputSchema>;
