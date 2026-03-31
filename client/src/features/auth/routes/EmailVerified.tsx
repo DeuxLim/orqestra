2416b6b5b42eecdfcad8d607dafeb2e42f14e380
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-31 20:12:52 +0800

    Email verification component

--- client/src/features/auth/routes/EmailVerified.tsx
+++ client/src/features/auth/routes/EmailVerified.tsx
@@ -1,22 +1,40 @@
 import AuthLayout from "@/components/layouts/AuthLayout";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import { useEffect } from "react";
+import { Progress } from "@/components/ui/progress";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 export default function EmailVerified() {
     const navigate = useNavigate();
+    const [loadingProgress, setLoadingProgress] = useState(100);
 
     useEffect(() => {
-        setTimeout(() => {
-            navigate("/orqestra/dashboard");
-        }, 5000);
-    });
+        const interval = setInterval(() => {
+            setLoadingProgress((prev) => {
+                if (prev <= 0) {
+                    clearInterval(interval);
+                    navigate("/orqestra/dashboard");
+                    return 0;
+                }
+                return prev - 1;
+            });
+        }, 50);
+
+        return () => clearInterval(interval);
+    }, [navigate]);
+
     return (
         <AuthLayout>
-            <Card className="w-110 text-center">
+            <Card className="text-center">
                 <CardHeader>
-                    <CardTitle>You account is now verified</CardTitle>
+                    <CardTitle>
+                        You account is now verified. <br /> Redirecting you to
+                        your workspace.
+                    </CardTitle>
                 </CardHeader>
+                <div className="px-10">
+                    <Progress value={loadingProgress} />
+                </div>
             </Card>
         </AuthLayout>
     );
