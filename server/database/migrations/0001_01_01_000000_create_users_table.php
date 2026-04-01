bed4ed7aea1147a3f1232456459c6b211207c595
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-01 21:04:14 +0800

    added is platform admin flag on users table

--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_platform_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
