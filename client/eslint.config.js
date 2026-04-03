1deb20957530aed3333b9453a9ccd0b52fb5692c
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-03 08:18:58 +0800

    eslint policy for naming conventions

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import checkFile from 'eslint-plugin-check-file'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -24,6 +25,31 @@ export default defineConfig([
     files: ['src/components/ui/**/*.{ts,tsx}', 'src/components/data-table.tsx'],
     rules: {
       'react-refresh/only-export-components': 'off',
+    },
+  },
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    plugins: {
+      'check-file': checkFile,
+    },
+    rules: {
+      'check-file/folder-naming-convention': [
+        'error',
+        {
+          'src/**/': 'KEBAB_CASE',
+        },
+      ],
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          'src/app/routes/**/*.{ts,tsx}': 'PASCAL_CASE',
+          'src/app/App*.tsx': 'PASCAL_CASE',
+          'src/components/layouts/*.{ts,tsx}': 'PASCAL_CASE',
+          'src/components/ui/*.{ts,tsx}': 'KEBAB_CASE',
+          'src/features/**/components/*.{ts,tsx}': 'KEBAB_CASE',
+          'src/{hooks,lib,types,utils}/**/*.{ts,tsx}': 'KEBAB_CASE',
+        },
+      ],
     },
   },
 ])
