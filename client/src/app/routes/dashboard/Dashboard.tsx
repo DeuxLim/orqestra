6935dbc70d27cd9edbf1472405f245bef6afa694
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-03 09:03:10 +0800

    used shadcn dashboard block with sample data

--- client/src/app/routes/dashboard/Dashboard.tsx
+++ client/src/app/routes/dashboard/Dashboard.tsx
@@ -1,14 +1,32 @@
-import { type ReactNode } from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import data from "@/data.json";
 
-type Props = {
-    children?: ReactNode;
-};
-
-export default function Dashboard({ children }: Props) {
+export default function Dashboard() {
     return (
-        <div>
-            <div>This is the dashboard</div>
-            <div>{children}</div>
-        </div>
+        <TooltipProvider>
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <SiteHeader />
+                    <div className="flex flex-1 flex-col">
+                        <div className="@container/main flex flex-1 flex-col gap-2">
+                            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                                <SectionCards />
+                                <div className="px-4 lg:px-6">
+                                    <ChartAreaInteractive />
+                                </div>
+                                <DataTable data={data} />
+                            </div>
+                        </div>
+                    </div>
+                </SidebarInset>
+            </SidebarProvider>
+        </TooltipProvider>
     );
 }
