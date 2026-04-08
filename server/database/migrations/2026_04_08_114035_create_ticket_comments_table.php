8705e67b24d9977cdc9c9599b4ed86de8e429b81
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-08 19:46:12 +0800

    migration : ticket comments

---
+++ server/database/migrations/2026_04_08_114035_create_ticket_comments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Customer;
+use App\Models\Ticket;
+use App\Models\User;
+use App\Models\Workspace;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ticket_comments', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(Workspace::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Ticket::class)->constrained()->cascadeOnDelete();
+
+            $table->foreignIdFor(User::class)
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->foreignIdFor(Customer::class)
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->text('body');
+            $table->boolean('is_internal')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket_comments');
+    }
+};
