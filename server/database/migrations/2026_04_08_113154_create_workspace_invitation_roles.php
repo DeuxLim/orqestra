e96353300a7a6ca15899757adb3fd5e2293533a6
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-08 19:45:53 +0800

    invitation roles

---
+++ server/database/migrations/2026_04_08_113154_create_workspace_invitation_roles.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\WorkspaceInvitation;
+use App\Models\WorkspaceRole;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workspace_invitation_roles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(WorkspaceInvitation::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(WorkspaceRole::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+
+            $table->unique([
+                'workspace_invitation_id',
+                'workspace_role_id'
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workspace_invitation_roles');
+    }
+};
