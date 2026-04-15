bcbfc1ac8fecefac22dae436adef12eb2a3de777
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-15 17:53:51 +0800

    workspace model relationships

--- server/app/Models/Workspace.php
+++ server/app/Models/Workspace.php
@@ -11,9 +11,41 @@ class Workspace extends Model
         'slug'
     ];
 
-    public function users()
+    /* Get workspace owner */
+    public function owner()
     {
-        return $this->belongsToMany(User::class, "workspace_user")
-            ->withPivot('role');
+        return $this->belongsTo(User::class, "owner_user_id");
+    }
+
+    public function members()
+    {
+        // Get workspace users - 2nd argument is needed because pivot table name is custom
+        // (workspace_user instead of default user_workspace) 
+        return $this->belongsToMany(User::class, 'workspace_user');
+    }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function customers()
+    {
+        return $this->hasMany(Customer::class);
+    }
+
+    public function roles()
+    {
+        return $this->hasMany(WorkspaceRole::class);
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(WorkspacePermission::class, 'workspace_role_permissions');
+    }
+
+    public function activityLogs()
+    {
+        return $this->hasMany(ActivityLog::class);
     }
 }
