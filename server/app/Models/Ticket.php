052f8b8fc769732659add3b7310c60b5dc7c0469
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-15 17:54:05 +0800

    ticket model relationships

--- server/app/Models/Ticket.php
+++ server/app/Models/Ticket.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    public function assignedUser()
+    {
+        return $this->belongsTo(User::class, 'assigned_to_user_id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id');
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function workspace()
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(TicketComment::class);
+    }
 }
