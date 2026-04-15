03dcf708b3324d533c44693a4637fd8f18060dd4
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-15 17:54:37 +0800

    workspace role model relationships

--- server/app/Models/WorkspaceRole.php
+++ server/app/Models/WorkspaceRole.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class WorkspaceRole extends Model
 {
-    //
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'workspace_user_roles');
+    }
+
+    public function workspaces()
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    public function workspacePermissions()
+    {
+        return $this->belongsToMany(WorkspacePermission::class, 'workspace_role_permissions');
+    }
 }
