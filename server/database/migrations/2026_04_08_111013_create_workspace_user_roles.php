8c862c251ed17dcba1fe7e54f178698b04f0a4be
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-18 10:11:12 +0800

    updated workspace user roles table

--- server/database/migrations/2026_04_08_111013_create_workspace_user_roles.php
+++ server/database/migrations/2026_04_08_111013_create_workspace_user_roles.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use App\Models\Workspace;
 use App\Models\WorkspaceRole;
+use App\Models\WorkspaceUser;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +17,7 @@ return new class extends Migration
     {
         Schema::create('workspace_user_roles', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(WorkspaceUser::class)->constrained()->cascadeOnDelete();
             $table->foreignIdFor(WorkspaceRole::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
 
