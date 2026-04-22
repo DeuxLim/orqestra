1f0794bc25c9906c2a8f793dc8d3ac4e36b96aef
Deux Daniel Lim <limdeux27@gmail.com>
2026-04-22 18:48:39 +0800

    minor update

--- client/src/app/routes/landing-page/LandingPage.tsx
+++ client/src/app/routes/landing-page/LandingPage.tsx
@@ -1,10 +1,13 @@
 import { type ReactNode } from "react";
+import { Navigate } from "react-router";
 
 type Props = {
     children?: ReactNode;
 };
 
 export default function LandingPage({ children }: Props) {
+    /* No landing page yet */
+    return <Navigate to="/login" />;
     return (
         <div>
             <div>This is the landing page</div>
